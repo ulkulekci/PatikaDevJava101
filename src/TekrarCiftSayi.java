5b72e7bff52f482452795a06f5865bdc2f386d92
ulkulekci <ulkusulu07@hotmail.com>
2022-12-29 16:04:32 +0300

    PATİKA DEV ODEV

---
+++ src/TekrarCiftSayi.java
@@ -0,0 +1,30 @@
+import java.util.Arrays;
+
+public class TekrarCiftSayi {
+    static boolean isFind(int[] arr, int value){
+        for (int i:arr){
+            if (i==value){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        int[] list={3,3,7,8,6,1,2,1,2,55,6,12,12};
+        int[] duplicate=new int[list.length];
+        int startIndex=0;
+        for(int i=0;i<list.length;i++){
+           for(int j=0;j<list.length;j++){
+            if ((i!=j)&&(list[i]==list[j])&&(list[i]%2==0)){
+                if (!isFind(duplicate,list[i])){
+             duplicate[startIndex++]=list[i];}
+             break;
+            }
+           }
+        }
+        for (int value:duplicate){
+            if (value !=0){
+        System.out.println(value);}
+    }}
+}
